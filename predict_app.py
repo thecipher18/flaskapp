e9533ce54246b3e09fc7854023d0b0aa6b1ec348
thecipher18 <45494203+CzApple1098@users.noreply.github.com>
2020-04-17 08:44:21 +0700

    /single

--- predict_app.py
+++ predict_app.py
@@ -12,8 +12,16 @@ from keras.preprocessing.image import img_to_array
 from flask import request
 from flask import jsonify
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'root'
+# app.config['MYSQL_DB'] = 'MyDB'
+
+mysql = MySQL(app)
 
 def get_model():
     global model 
@@ -61,9 +69,47 @@ def predict():
     model = tf.keras.models.load_model('kerasmodel.h5')
     prediction = model.predict(processed_image).tolist()
     print("predicted")
+
     response = {
         'result': CATEGORIES[prediction[0].index(max(prediction[0]))],
         'prediction': max(prediction[0])
+    }
+    return jsonify(response)
+
+
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#     message = request.get_json(force=True)
+#     name = message['name']
+#     response = {
+#         'greeting': 'Hello' + name
+#     }
+#     return jsonify(response)
+
+@app.route('/single', methods=["POST"])
+def single():
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(28,28))
+    # model.predict():
+    # with graph.as_default():
+    model = tf.keras.models.load_model('kerasmodel.h5')
+    prediction = model.predict(processed_image).tolist()
+    print("predicted")
+
+    time = message["time"]
+    answer = message["answer"]
+
+
+    index = CATEGORIES.index(answer)
+    modelacc = prediction[0][index]
+    myScore = time * modelacc
+
+    response = {
+        'modelAnswer': CATEGORIES[prediction[0].index(max(prediction[0]))],
+        'score': myScore,
     }
     return jsonify(response)
 
