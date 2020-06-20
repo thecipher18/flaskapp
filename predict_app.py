4379294195c7bccb61398ec1a60ec489a15aabdc
thecipher18 <45494203+CzApple1098@users.noreply.github.com>
2020-06-20 19:02:13 +0700

    change image processing

--- predict_app.py
+++ predict_app.py
@@ -13,7 +13,7 @@ from flask import request
 from flask import jsonify
 from flask import Flask
 from flask_mysqldb import MySQL
-
+from cv2 import cv2
 app = Flask(__name__)
 
 # app.config['MYSQL_HOST'] = 'localhost'
@@ -29,21 +29,97 @@ def get_model():
     print("2")
     print(" * Model loaded!")
 
-def preprocess_image(image, target_size):
+
+
+def deletenp(data, top, right, bot, left):
+    data = data
+    for i in range(top):
+        data = np.delete(data, 0, 0)
+        
+    data = np.rot90(data)
+    for i in range(right):
+        data = np.delete(data, 0, 0)
+      
+    data = np.rot90(data)
+    for i in range(bot):
+        data = np.delete(data, 0, 0)
+        
+    data = np.rot90(data)
+    for i in range(left):
+        data = np.delete(data, 0, 0)
+        
+    data = np.rot90(data)
+    return data
+
+def check255(array):
+    for i in array:
+            if(i != 255):
+                return 1
+
+def crop(data):
+    #crop top
+    top = 0
+    for i in data: 
+        if(check255(i)):
+            break
+        else:
+            top += 1
+            continue
+            
+    #crop right
+    rotright = np.rot90(data)
+    right = 0
+    for i in rotright:
+        if(check255(i)):
+            break
+        else:
+            right += 1
+            continue
+            
+    #crop bottom
+    bot = 0
+    rotbot = np.rot90(rotright)
+    for i in rotbot:
+        if(check255(i)):
+            break
+        else:
+            bot += 1
+            continue
+            
+    #crop left
+    left = 0
+    rotleft = np.rot90(rotbot)
+    for i in rotleft:
+        if(check255(i)):
+            break
+        else:
+            left += 1
+            continue
+    minval =  min(top, right, left, bot)       
+    crop_array = deletenp(data, minval, minval, minval, minval)
+    return crop_array
+
+def preprocess_image(image):
     print("3")
+    IMG_SIZE = 28 
     if image.mode == "RGB":
         image = image.convert("L")
     print(image)
-    image = image.resize(target_size)
-    print("image array:", image)
-    print("8", type(image))
-    image = img_to_array(image)
-    print("9", image.shape)
-    image = np.expand_dims(image, axis = 0)
-    print("image shape after expand dims", image.shape)
-    # image2 = np.expand_dims(image, 1)
-    # print("10", image.shape," image2: ", image2.shape)
-    return image
+    cv_image = np.array(image)
+    print(cv_image)
+    img_array = crop(cv_image)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE), interpolation = cv2.INTER_AREA)
+    # image = image.resize(target_size)
+    # print("image array:", image)
+    # print("8", type(image))
+    # image = img_to_array(image)
+    # print("9", image.shape)
+    # image = np.expand_dims(image, axis = 0)
+    # print("image shape after expand dims", image.shape)
+    # # image2 = np.expand_dims(image, 1)
+    # # print("10", image.shape," image2: ", image2.shape)
+    # return image
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)  
 
 print(" Loading Keras model...")
 get_model()
@@ -66,7 +142,7 @@ def predict():
     decoded = base64.b64decode(encoded)
     image = Image.open(io.BytesIO(decoded))
     print(type(image))
-    processed_image = preprocess_image(image, target_size=(28,28))
+    processed_image = preprocess_image(image)
     print("here")
     # model.predict():
     # with graph.as_default():
@@ -96,7 +172,7 @@ def single():
     encoded = message['image']
     decoded = base64.b64decode(encoded)
     image = Image.open(io.BytesIO(decoded))
-    processed_image = preprocess_image(image, target_size=(28,28))
+    processed_image = preprocess_image(image)
     # model.predict():
     # with graph.as_default():
     model = tf.keras.models.load_model('kerasmodel.h5')
